7c69c3fc3d8187be7dc8bc3d29ba90ff4afe79f9
Ernest <arreern@gmail.com>
2019-09-26 20:53:33 -0700

    ..

--- models/business.js
+++
@@ -1,9 +0,0 @@
-class Business { 
-
-    constructor(id, title) {
-        this.id = id; 
-        this.title = title;
-    }
-}
-
-export default Business; 
--- store/business-actions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_BUSINESS = 'ADD_BUSINESS'; 
-
-export const addBusiness = title => {
-    return { type: ADD_BUSINESS, businessData: { title: title } };
-};
