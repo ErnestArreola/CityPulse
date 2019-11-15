c0adaaffb1a3550382008f2ceb2b802157b922af
ErnestArreola <47127673+ErnestArreola@users.noreply.github.com>
2019-11-15 12:47:33 -0800

    Pulse is Frontend

---
+++ Pulse/src/pages/Welcome.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Card, Typography, Alert } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+
+const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre
+    style={{
+      background: '#f2f4f5',
+      padding: '12px 20px',
+      margin: '12px 0',
+    }}
+  >
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default (): React.ReactNode => (
+  <PageHeaderWrapper>
+    <Card>
+      <Alert
+        message="Pulse Home Page"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
+    </Card>
+    <p style={{ textAlign: 'center', marginTop: 24 }}>
+      Want to add more pages? Please refer to{' '}
+      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+        use block
+      </a>
+      。
+    </p>
+  </PageHeaderWrapper>
+);
