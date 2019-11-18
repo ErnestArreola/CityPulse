ca805be0c371d7af0245c3a74f087b643252b000
ErnestArreola <47127673+ErnestArreola@users.noreply.github.com>
2019-11-18 14:10:40 -0800

    Updated and Merged

--- Pulse/config/config.ts
+++ Pulse/config/config.ts
@@ -6,7 +6,6 @@ import webpackPlugin from './plugin.config';
 const { pwa, primaryColor } = defaultSettings;
 
 // preview.pro.ant.design only do not use in your production ;
-// preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
 const plugins: IPlugin[] = [
